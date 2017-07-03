cbccc880b9f36a49f19e42fe3e24c23b1fe14b06
Maria Rumyantseva <rumyantseva.mary@gmail.com>
2017-07-03 18:31:31 +0300

    clone Node Chat

---
+++ auth.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var passport = require("passport");
+var _ = require("lodash");
+var users = require("./data/users.json");
+var router = express.Router();
+module.exports = router;
+
+router.get("/login", function (req, res) {
+    if (req.app.get("env") === "development") {//код чтобы быть постоянно зарегистрированным
+        var user = users[0];
+        if (req.query.user) {
+            user = _.find(users, u => u.name == req.query.user)
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            return res.redirect('/');
+        });
+        return;
+    }
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    }));
+
+router.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/login");
+});
