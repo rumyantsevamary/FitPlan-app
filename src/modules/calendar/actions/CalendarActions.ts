5f5bf3a7a4e0fe6d38d0362bdd5e5dfde3539c39
maria <rumyantseva.mary@gmail.com>
2019-12-13 00:50:54 +0300

    Uninstalling axios

--- src/modules/calendar/actions/CalendarActions.ts
+++ src/modules/calendar/actions/CalendarActions.ts
@@ -5,7 +5,6 @@ import {
   GET_PLANNED_EVENTS_FAIL,
   OPEN_DAY_DETAILS
 } from '../constants/Calendar';
-import axios from 'axios';
 
 export function changeMonth(month: any, year: any) {
   return (dispatch: Function) => {
@@ -19,31 +18,31 @@ export function changeMonth(month: any, year: any) {
   };
 }
 
-export function getPlannedEvents(year: any, month: any) {
-  return (dispatch: Function) => {
-    dispatch({
-      type: GET_PLANNED_EVENTS_REQUEST
-    });
-    axios
-      .post('/api/plannedActions', {
-        year: year,
-        month: month
-      })
-      .then(function(data) {
-        dispatch({
-          type: GET_PLANNED_EVENTS_SUCCESS,
-          playload: data
-        });
-      })
-      .catch((textStatus: string) => {
-        dispatch({
-          type: GET_PLANNED_EVENTS_FAIL,
-          payload: textStatus,
-          error: true
-        });
-      });
-  };
-}
+// export function getPlannedEvents(year: any, month: any) {
+//   return (dispatch: Function) => {
+//     dispatch({
+//       type: GET_PLANNED_EVENTS_REQUEST
+//     });
+//     axios
+//       .post('/api/plannedActions', {
+//         year: year,
+//         month: month
+//       })
+//       .then(function(data) {
+//         dispatch({
+//           type: GET_PLANNED_EVENTS_SUCCESS,
+//           playload: data
+//         });
+//       })
+//       .catch((textStatus: string) => {
+//         dispatch({
+//           type: GET_PLANNED_EVENTS_FAIL,
+//           payload: textStatus,
+//           error: true
+//         });
+//       });
+//   };
+// }
 
 export function openDayDetails(day: any) {
   return (dispatch: Function) => {
--- src/modules/mainMenu/actions/mainMenuActions.ts
+++ src/modules/mainMenu/actions/mainMenuActions.ts
@@ -5,7 +5,6 @@ import {
   SET_ACTIVE_CONTENT,
   GET_MAIN_MENU_CANCELLED
 } from '../constants/mainMenuActionTypes';
-import axios from 'axios';
 
 export const getMainMenuBegin = () => ({
   type: GET_MAIN_MENU_BEGIN
@@ -23,29 +22,6 @@ export const getMainMenuFail = () => ({
 export const getMainMenuCancelled = () => ({
   type: GET_MAIN_MENU_CANCELLED
 });
-
-export function getMainMenu() {
-  return (dispatch: Function) => {
-    dispatch({
-      type: GET_MAIN_MENU_BEGIN
-    });
-    axios
-      .get('/api/mainMenu')
-      .then(function(request: any) {
-        dispatch({
-          type: GET_MAIN_MENU_SUCCESS,
-          playload: request.data[0]
-        });
-      })
-      .catch((textStatus: string) => {
-        dispatch({
-          type: GET_MAIN_MENU_FAIL,
-          payload: textStatus,
-          error: true
-        });
-      });
-  };
-}
 
 export const setActiveContent = (activeContent: any) => ({
   type: SET_ACTIVE_CONTENT,
